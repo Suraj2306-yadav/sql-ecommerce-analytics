e6fff87b578260bb319ab1484afad71785f6e04b
Suraj  Kumar <suraj.yadav.417330@gmail.com>
2026-03-02 19:50:10 +0530

    add carg% function

--- revenue_trend/yearly_revenue_growth.sql
+++ revenue_trend/yearly_revenue_growth.sql
@@ -18,3 +18,15 @@ create table if not exists seventh-palace-451715-i9.ecommerce_analysing.yearly_r
     lag(yearly_revenue) over(order by year))*100,2) as yearly_growth_percentage
   from yearly_revenues
   );
+
+# this is find the carg% helps for understand the actual growth_rate of a business
+with yearly_revenues as (
+select year,sum(total_revenue) as yearly_revenue
+from `seventh-palace-451715-i9.ecommerce_analysing.monthly_revenue_growth`
+group by year)
+
+select 
+ pow(
+  max(yearly_revenue)/min(yearly_revenue),1.0/(max(year)-min(year))
+  ) - 1 as cagr
+from yearly_revenues; 
